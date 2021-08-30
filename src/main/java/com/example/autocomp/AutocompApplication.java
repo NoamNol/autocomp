8c4f9a036f2345799708ebfc139709517bf5a6c0
NoamNol <noamnol19@gmail.com>
2021-08-30 17:45:30 +0300

    Init Spring

---
+++ src/main/java/com/example/autocomp/AutocompApplication.java
@@ -0,0 +1,13 @@
+package com.example.autocomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocompApplication.class, args);
+	}
+
+}
